20ccff5f78b11b7cfc3a7f0d3dd8058a6f612f55
Sarah <safriedman@ma.org>
2017-09-18 17:27:47 -0700

    hw

--- LetterCountHistorgram_SarahF.py
+++ LetterCountHistorgram_SarahF.py
@@ -2,10 +2,9 @@
 #Sarah Friedman
 #9/18/17
 
-#NOT FINISHED
-
 import turtle
 t = turtle.Turtle()
+t2 = turtle.Turtle()
 wn = turtle.Screen()
 wn.setworldcoordinates(-400, -400, 400, 400)
 
@@ -18,7 +17,9 @@ letterVal = []
 
 dictionary = {}
 
-def drawChart(t):
+def drawChart(t, tt):
+    tt.ht()
+
     t.penup()
     t.backward(300)
     t.right(90)
@@ -27,11 +28,21 @@ def drawChart(t):
     t.forward(600)
     t.left(90)
 
+    tt.penup()
+    tt.goto(-300, -315)
+
     x = 0
-    for letter in range(26):
+
+    t.forward(11.5)
+    tt.forward(11.5)
+    t.dot(5)
+    tt.write(alphabet[x])
+    x = x+1
+    for letter in range(25):
         t.forward(23)
+        tt.forward(23)
         t.dot(5)
-        t.write(alphabet[x])
+        tt.write(alphabet[x])
         x = x+1
     t.ht()
 
@@ -48,13 +59,10 @@ def countAppearance():
     for l in range(26):
         letter = alphabet[x]
         if letter in dictionary:
-#            print("Y: ", letter)
             letterVal.append(dictionary[letter])
-#            print(letterVal)
             x = x+1
         else:
             letterVal.append(0)
-#            print("Y: ", y)
             x = x+1
     return letterVal
 
@@ -67,20 +75,23 @@ def draw(val, t):
     t.penup()
     t.goto(-300, -300)
     t.pendown()
+
     for x in val:
         if x > 0:
-            t.forward(23)
+            t.forward(11.5)
             t.left(90)
-            t.forward((val[x])*10)
-            t.write(val[x])
-            t.backward((val[x])*10)
+            t.forward((x)*15)
+            t.write(x)
             t.right(90)
+            t.forward(11.5)
+            t.right(90)
+            t.forward((x)*15)
+            t.left(90)
             x = x+1
-#        t.right(90)
-#        t.forward(22)
         else:
             t.forward(23)
             x = x+1
+    t.ht()
 
 
 
@@ -88,7 +99,7 @@ countAll(string)
 countAppearance()
 print(letterVal)
 
-drawChart(t)
+drawChart(t, t2)
 draw(letterVal, turtle)
 
 wn.exitonclick()
