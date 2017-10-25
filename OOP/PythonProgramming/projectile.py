81764918577af1688063011360d6dc4d1e03e8a9
Sarah <safriedman@ma.org>
2017-10-24 17:38:08 -0700

    hw

--- OOP/PythonProgramming/projectile.py
+++ OOP/PythonProgramming/projectile.py
@@ -15,9 +15,12 @@ class Projectile:
         self.xvel = velocity * math.cos(theta)
         self.yvel = velocity * math.sin(theta)
 
-    def update(self, time):
+    def update(self, time, height):
         self.xpos = self.xpos + time * self.xvel
         yvel1 = self.yvel - time * 9.8
+        yval = str(yvel1)
+        height = height.append(yval)
+
         self.ypos = self.ypos + time * (self.yvel + yvel1)/2.0
         self.yvel = yvel1
 
@@ -26,21 +29,3 @@ class Projectile:
 
     def getY(self):
         return self.ypos
-
-def getInputs():
-    a = eval(input("Enter the launch angle (in degrees): "))
-    v = eval(input("Enter the initial velocity (in meters/sec): "))
-    h = eval(input("Enter the initial height (in meters): "))
-    t = eval(input("Enter the time interval between position calculations: "))
-
-    return a, v, h, t
-
-
-def main():
-    angle, vel, h0, time = getInputs()
-    cball = Projectile(angle, vel, h0)
-    while cball.getY() >= 0:
-        cball.update(time)
-    print("\nDistance traveled: {0:0.1f} meters".format(cball.getX()))
-
-main()
